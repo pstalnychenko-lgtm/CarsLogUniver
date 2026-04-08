ff515c794cabbf89b76cb06a93f09e43f9246e5c
pstalnychenko-lgtm <pstalnychenko@gmail.com>
2026-04-08 23:48:24 +0300

    WORCING with prog , from too many roads

---
+++ CarsLogWorkigVS/Interfaces/ISetFuelType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CarsLogWorkig.Models;
+
+namespace CarsLogWorkigVS.Interfaces
+{
+    public interface ISetFuelType
+    {
+        void SetFuelType(FuelsType fuelType);
+    }
+}
